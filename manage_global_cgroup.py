37be57425cce5fe1a4ff9bd24c60ecf61bb8bdbe
Saravanan Pandiyan <saravanan.pandiyan@nutanix.com>
2025-01-29 07:27:16 +0000

    Adding definition of global_cgroup_limit_calculator() function

--- manage_global_cgroup.py
+++ manage_global_cgroup.py
@@ -1,8 +1,45 @@
 # manage_global_cgroup.py
+# Most of the global OOM cases are recorded when min watermark is around 15-17% short of free mark.
+# So setting the alloc thresholod to 20% of ahead of min mark.
+global_oom_thrshld = 20#%
+alloc_thrshld_bytes = 0
 
+# Fetches the min and low water mark values.
+# Calculate the safer bytes to allocate so that we don't hit global OOM.
+# Returns: Safer bytes that can be used for cgroup alloc.
 def global_cgroup_limit_calculator():
-    """Placeholder for calculating global cgroup limits."""
-    pass
+    try:
+        normal_zinfo = "grep -A10 Normal /proc/zoneinfo"
+        pattern = "grep -E 'pages free|min'"
+        cmd = f"{normal_zinfo} | {pattern}"
+        ret = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+
+        pages_free = min_mark = None
+        for line in ret.stdout.splitlines():
+            match = re.search(r'pages free\s+(\d+)', line)
+            if match:
+                pages_free = int(match.group(1))
+            match = re.search(r'min\s+(\d+)', line)
+            if match:
+                min_mark = int(match.group(1))
+
+        if pages_free is None or min_mark is None:
+            raise ValueError("Could not extract values")
+
+        print(f"Pages Free: {pages_free}, Min: {min_mark}")
+        if pages_free > min_mark:
+            min_percentage = (min_mark / pages_free) * 100
+        print(f"% to min_mark: {min_percentage:.2f}")
+        alloc_thrshld_prct = min_percentage - global_oom_thrshld
+        alloc_thrshld_bytes = int(((alloc_thrshld_prct * pages_free)/100))*4000
+        print(f"Safer % short of global OOM: {alloc_thrshld_prct:.2f}")
+        print(f"Alloc bytes short of global oom: {alloc_thrshld_bytes}")
+        return alloc_thrshld_bytes
+
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failed: {e}")
+    except ValueError as e:
+        print(f"Parsing error: {e}")
 
 def allocate_mem_from_global_cgroup(amount_of_memory):
     """Placeholder for allocating memory from the global cgroup."""
